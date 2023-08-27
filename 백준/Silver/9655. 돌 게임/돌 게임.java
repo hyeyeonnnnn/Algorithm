d18547d34b9eced01ba4c8d11e02f66e1dd64ac2
hyeyeonnnnn <115079024+hyeyeonnnnn@users.noreply.github.com>
2023-08-28 03:30:24 +0900

    [Silver V] Title: 돌 게임, Time: 128 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -9,26 +9,12 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] dp;
-	static int result(int n) {
-		if(n==1)
-			return 1;
-		else if(n==2)
-			return 2;
-		else if(n==3)
-			return 1;
-		
-		if(dp[n]==0) {
-			dp[n] = result(n-3)+result(n-1);		
-		}
-		return dp[n];
-	}
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
-		dp = new int[n+1];
-		
+
 		if(n%2==0)
 			System.out.println("CY");
 		else
