ffa5c30861d36b157d7494eb8131e753b8fdd338
hyeyeonnnnn <115079024+hyeyeonnnnn@users.noreply.github.com>
2023-09-19 03:40:06 +0900

    [Silver I] Title: 단지번호붙이기, Time: 144 ms, Memory: 15920 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,73 +2,60 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
-
 
 public class Main {
 	static int N;
 	static int[][] graph;
-	static boolean[][] visited;
-	static int[] dx = {0, 0, -1, 1};
-	static int[] dy = {1, -1, 0, 0};
-	static int cnt = 0;
-	
+	static boolean[][] check;
+	static int cnt = 1;	
+	static int[] dx = {1,-1,0,0};
+	static int[] dy = {0,0,1,-1};
 	static void dfs(int x, int y) {
-		visited[x][y] = true;
-		cnt++;
+		check[x][y] = true;
+
 		for(int i=0;i<4;i++) {
-			int a = x+dx[i];
-			int b = y+dy[i];
-			
-			if(a>0 && b>0 && a<=N && b<=N) {
-				if(!visited[a][b] && graph[a][b]==1) {
-					dfs(a,b);
-				}
-			}
-			
+			int newX = x + dx[i];
+			int newY = y + dy[i];
+			if(newX>=0&&newX<N && newY>=0&&newY<N && graph[newX][newY]==1 &&!check[newX][newY]) {
+				cnt++;
+				dfs(newX, newY);
+			}	
 		}
-		
 	}
-	
-	
-	public static void main(String[] args) throws NumberFormatException, IOException  {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-				
+
 		N = Integer.parseInt(br.readLine());
+		graph = new int[N][N];
+		check = new boolean[N][N];
 		
-		graph = new int[N+1][N+1];
-		visited = new boolean[N+1][N+1];
-		
-		for(int i=1;i<=N;i++) {
-			String map = br.readLine();
-			for(int j=1;j<=N;j++) {
-				graph[i][j] = map.charAt(j-1)-'0';
+		for(int i=0;i<N;i++) {
+			String input = br.readLine();
+			for(int j=0;j<N;j++) {
+				graph[i][j] = input.charAt(j)-'0';
 			}
 		}
 		
-		int total = 0;
-		ArrayList<Integer> home = new ArrayList<>();
-		for(int x=1;x<=N;x++) {
-			for(int y=1;y<=N;y++) {
-				if(!visited[x][y] && graph[x][y]==1) {
-					dfs(x, y);
-					total++;
-					home.add(cnt);
-					cnt=0;
+		ArrayList<Integer> list = new ArrayList<>();
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+				if(!check[i][j] && graph[i][j]==1) {
+					dfs(i, j);
+					list.add(cnt);
+					cnt = 1;
 				}
 			}
 		}
 		
-		sb.append(total+"\n");
-		Collections.sort(home);
-		for(int i=0;i<home.size();i++) {
-			sb.append(home.get(i)+"\n");
-		}
-		
+		sb.append(list.size()+"\n");
+		Collections.sort(list);
+		for(int i=0;i<list.size();i++)
+			sb.append(list.get(i)+"\n");
 		System.out.println(sb);
-	}
+	}	
 }
+
